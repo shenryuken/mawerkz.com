e798d3ba9a2eb8ca443a424d1365db781fa49928
shenryuken <shenryuken@yahoo.com>
2018-10-28 03:56:37 +0800

    First commit

---
+++ resources/views/mawerkz.blade.php
@@ -0,0 +1,241 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Mawerkz</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="{{asset('arcana/assets/css/main.css')}}" />
+	</head>
+	<body class="is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+
+					<!-- Logo -->
+						<h1><a href="index.html" id="logo">Mawerkz <em>by MA TRADING</em></a></h1>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li class="current"><a href="index.html">Home</a></li>
+								<!-- <li>
+									<a href="#">Dropdown</a>
+									<ul>
+										<li><a href="#">Lorem dolor</a></li>
+										<li><a href="#">Magna phasellus</a></li>
+										<li><a href="#">Etiam sed tempus</a></li>
+										<li>
+											<a href="#">Submenu</a>
+											<ul>
+												<li><a href="#">Lorem dolor</a></li>
+												<li><a href="#">Phasellus magna</a></li>
+												<li><a href="#">Magna phasellus</a></li>
+												<li><a href="#">Etiam nisl</a></li>
+												<li><a href="#">Veroeros feugiat</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Veroeros feugiat</a></li>
+									</ul>
+								</li> -->
+								<li><a href="#">SuperBike</a></li>
+								<li><a href="#">Bike</a></li>
+								<li><a href="#">Scooter</a></li>
+								<li><a href="#">Parts And Accessories</a></li>
+							</ul>
+						</nav>
+
+				</div>
+
+			<!-- Banner -->
+				<section id="banner">
+					<!-- <header>
+						<h2>Arcana: <em>A responsive site template freebie by <a href="http://html5up.net">HTML5 UP</a></em></h2>
+						<a href="#" class="button">Learn More</a>
+					</header> -->
+				</section>
+
+			<!-- Highlights -->
+				<section class="wrapper style1">
+					<div class="container">
+						<div class="row gtr-200">
+							<section class="col-4 col-12-narrower">
+								<div class="box highlight">
+									<i class="icon major fa-paper-plane"></i>
+									<h3>This Is Important</h3>
+									<p>MA TRADING is Malaysia top supplier of parts and accessories for vintage and modern scooters. We have everything you need, from wear-and-tear items to aftermarket parts from brands like Malossi, Ermax, Bitubo, Grip Lock, FA Italia and more.
+</p>
+								</div>
+							</section>
+							<section class="col-4 col-12-narrower">
+								<div class="box highlight">
+									<i class="icon major fa-pencil"></i>
+									<h3>Also Important</h3>
+									<p>MA TRADING, is a dedicated bike products importer, wholesaler and distributor to the Malaysian scooters retail market since 2007.</p>
+								</div>
+							</section>
+							<section class="col-4 col-12-narrower">
+								<div class="box highlight">
+									<i class="icon major fa-wrench"></i>
+									<h3>Probably Important</h3>
+									<p> We offer a wide range of scooters products, which includes replacement parts, performance parts, scooter accessories, scooter tools, scooter manuals, essentials like oils, tires, tubes, or for lifestyle items.
+									</p>
+								</div>
+							</section>
+						</div>
+					</div>
+				</section>
+
+			<!-- Gigantic Heading -->
+				<section class="wrapper style2">
+					<div class="container">
+						<header class="major">
+							<h2>World Leading Brand With Us!</h2>
+							<p>MA Trading  offers a wide range of aftermarket scooter performance upgrades to help make your scooter handle better, accelerate faster, rev quicker, stop harder, and go faster!  We are the exclusive Malaysia importers for high-quality performance scooter parts brands! </p>
+						</header>
+					</div>
+				</section>
+
+			<!-- Posts -->
+				<section class="wrapper style1">
+					<div class="container">
+						<div class="row">
+							<section class="col-6 col-12-narrower">
+								<div class="box post">
+									<a href="#" class="image left"><img src="{{asset('arcana/images/pic01.jpg')}}" alt="" /></a>
+									<div class="inner">
+										<h3>The First Thing</h3>
+										<p>We strives for a continuous effort to improve and Professionalism is always our hallmark. </p>
+									</div>
+								</div>
+							</section>
+							<section class="col-6 col-12-narrower">
+								<div class="box post">
+									<a href="#" class="image left"><img src="{{asset('arcana/images/pic02.jpg')}}" alt="" /></a>
+									<div class="inner">
+										<h3>The Second Thing</h3>
+										<p>Duis neque nisi, dapibus sed mattis et quis, nibh. Sed et dapibus nisl amet mattis, sed a rutrum accumsan sed. Suspendisse eu.</p>
+									</div>
+								</div>
+							</section>
+						</div>
+						<div class="row">
+							<section class="col-6 col-12-narrower">
+								<div class="box post">
+									<a href="#" class="image left"><img src="{{asset('arcana/images/pic03.jpg')}}" alt="" /></a>
+									<div class="inner">
+										<h3>The Third Thing</h3>
+										<p>Duis neque nisi, dapibus sed mattis et quis, nibh. Sed et dapibus nisl amet mattis, sed a rutrum accumsan sed. Suspendisse eu.</p>
+									</div>
+								</div>
+							</section>
+							<section class="col-6 col-12-narrower">
+								<div class="box post">
+									<a href="#" class="image left"><img src="{{asset('arcana/images/pic04.jpg')}}" alt="" /></a>
+									<div class="inner">
+										<h3>The Fourth Thing</h3>
+										<p>Duis neque nisi, dapibus sed mattis et quis, nibh. Sed et dapibus nisl amet mattis, sed a rutrum accumsan sed. Suspendisse eu.</p>
+									</div>
+								</div>
+							</section>
+						</div>
+					</div>
+				</section>
+
+			<!-- CTA -->
+				<section id="cta" class="wrapper style3">
+					<div class="container">
+						<header>
+							<h2>Are you ready to continue your quest?</h2>
+							<a href="#" class="button">Coming Soon (Ecommerce)</a>
+						</header>
+					</div>
+				</section>
+
+			<!-- Footer -->
+				<div id="footer">
+					<div class="container">
+						<div class="row">
+							<section class="col-6 col-6-narrower col-12-mobilep">
+								<h3>MA TRADING Address</h3>
+								<!-- <ul class="links">
+									<li><a href="#">Mattis et quis rutrum</a></li>
+									<li><a href="#">Suspendisse amet varius</a></li>
+									<li><a href="#">Sed et dapibus quis</a></li>
+									<li><a href="#">Rutrum accumsan dolor</a></li>
+									<li><a href="#">Mattis rutrum accumsan</a></li>
+									<li><a href="#">Suspendisse varius nibh</a></li>
+									<li><a href="#">Sed et dapibus mattis</a></li>
+								</ul> -->
+								<p>MA TRADING 05/AD Menara Pandan A, Jalan Pandan Utama 55100 Kuala Lumpur Malaysia </p>
+							</section>
+							<section class="col-6 col-6-narrower col-12-mobilep">
+								<h3>Contact Information</h3>
+								<ul class="links">
+									<li><a href="#">Tel : +603 4287 4957 </a></li>
+									<li><a href="#">H/P : +6019 268 8063</a></li>
+									<li><a href="#">Fax : +603 4292 9989 </a></li>
+									<li><a href="#">Email :</a></li>
+									<li><a href="#">hazriell@gmail.com</a></li>
+									<li><a href="#">matrading08@yahoo.com.my</a></li>
+								</ul>
+							</section>
+							<!-- <section class="col-6 col-12-narrower">
+								<h3>Get In Touch</h3>
+								<form>
+									<div class="row gtr-50">
+										<div class="col-6 col-12-mobilep">
+											<input type="text" name="name" id="name" placeholder="Name" />
+										</div>
+										<div class="col-6 col-12-mobilep">
+											<input type="email" name="email" id="email" placeholder="Email" />
+										</div>
+										<div class="col-12">
+											<textarea name="message" id="message" placeholder="Message" rows="5"></textarea>
+										</div>
+										<div class="col-12">
+											<ul class="actions">
+												<li><input type="submit" class="button alt" value="Send Message" /></li>
+											</ul>
+										</div>
+									</div>
+								</form>
+							</section> -->
+						</div>
+					</div>
+
+					<!-- Icons -->
+						<ul class="icons">
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+							<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+							<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+						</ul>
+
+					<!-- Copyright -->
+						<div class="copyright">
+							<ul class="menu">
+								<li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+							</ul>
+						</div>
+
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="{{ asset('arcana/assets/js/jquery.min.js')}}"></script>
+			<script src="{{ asset('arcana/assets/js/jquery.dropotron.min.js')}}"></script>
+			<script src="{{ asset('arcana/assets/js/browser.min.js')}}"></script>
+			<script src="{{ asset('arcana/assets/js/breakpoints.min.js')}}"></script>
+			<script src="{{ asset('arcana/assets/js/util.js')}}"></script>
+			<script src="{{ asset('arcana/assets/js/main.js')}}"></script>
+
+	</body>
+</html>
